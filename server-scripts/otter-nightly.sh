b611f7fc66715f8194ee7343a33c0638fb3d6f69
M Kunde <mikolaj.kundegorski@gmail.com>
2023-05-08 16:24:29 +0100

    use superuser

--- server-scripts/otter-nightly.sh
+++ server-scripts/otter-nightly.sh
@@ -3,7 +3,7 @@ date
 sudo -u postgres psql -f init.sql
 cd ../input
 # copying on local network from weasel
-rsync --append --partial -chvP -e "ssh" mix@192.168.0.7:/home/habis/coredb_bup.tar.gz .
+rsync --append --partial -chvP -e "ssh" root@192.168.0.7:/home/habis/coredb_bup.tar.gz .
 tar xf coredb_bup.tar.gz
 sudo -u postgres psql -d coredb -f coredb_bup.sql
 sudo -u postgres psql -d bahistot -f bahistot.sql
