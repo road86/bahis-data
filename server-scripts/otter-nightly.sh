0a244162a96161c373e1f6dbcd59fe93ae41441b
M Kunde <mikolaj.kundegorski@gmail.com>
2023-05-23 12:56:04 +0100

    minor corrections

--- server-scripts/otter-nightly.sh
+++ server-scripts/otter-nightly.sh
@@ -10,8 +10,10 @@ rsync --append --partial -chvP -e "ssh" root@192.168.0.7:/home/habis/coredb_bup.
 tar xf coredb_bup.tar.gz
 sudo -u postgres psql -d coredb -f coredb_bup.sql
 sudo -u postgres psql -d bahistot -f bahistot.sql
-cd ..
+cd /bahis-data/
 /bahis-data/.venv/bin/python server-scripts/import_data.py
 /bahis-data/.venv/bin/python prep_dash/prepgeojson.py
 /bahis-data/.venv/bin/python prep_dash/prep_data.py
+cp -r /bahis-data/output/* /bahis-dash/exported_data/
+cd /bahis-dash/
 /bahis-dash/.venv/bin/python /bahis-dash/index.py
