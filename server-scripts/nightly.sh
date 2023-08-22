3247cef60428848e9c8b9b403ab2f74c3847db59
M Kunde <mikolaj.kundegorski@gmail.com>
2023-08-22 11:31:47 +0100

    make it run correctly in docker

--- server-scripts/nightly.sh
+++ server-scripts/nightly.sh
@@ -1,13 +1,13 @@
 set -xe
 date
-sudo -u postgres psql -f init.sql
+psql -U postgres -h dash-db -f init.sql
 cd ../input
 # copying local file 
-cp /home/bahis/backups/coredb_bup.tar.gz .
+cp /bahis-backup/coredb_bup.tar.gz .
 tar xf coredb_bup.tar.gz
-sudo -u postgres psql -d coredb -f coredb_bup.sql
+psql -U postgres -h dash-db -d coredb -f coredb_bup.sql
 cd /bahis-data/
-/bahis-data/.venv/bin/python server-scripts/import_data.py
-/bahis-data/.venv/bin/python prep_dash/prepgeojson.py
-/bahis-data/.venv/bin/python prep_dash/prep_data.py
+python3 server-scripts/import_data.py
+python3 prep_dash/prepgeojson.py
+python3 prep_dash/prep_data.py
 cp -r /bahis-data/output/* /bahis-dash/exported_data/
